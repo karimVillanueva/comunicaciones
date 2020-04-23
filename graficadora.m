5c47a52d26e2f8785dc0b621f059a7e48017d1dc
kgv <kgv@192.168.1.67>
2020-04-23 16:53:26 -0500

    add properties to plot

--- graficadora.m
+++ graficadora.m
@@ -2,3 +2,6 @@ x = -10: 0.01:10;
 disp('Este programa gráfica una función')
 f = input('Ingrese una función en terminos de x, no olvide ./ .* .- .+   cuando sea necesario \n');
 plot(x,f)
+title('Grafica de f')
+xlabel('tiempo [s]')
+ylabel('función')
